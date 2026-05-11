fef1583d3ac53cb4e822ffd4c988f4a1bc8d7466
Zyft23 <ariazulvanrc@gmail.com>
2026-05-11 03:39:22 +0000

    test2

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-from sistem import oksigen, laboratorium
+from sistem import RuangOksigen, laboratorium
+modul = {
+    "ruang1": RuangOksigen(tekanan_udara=101.3, cadangan_oksigen=500, oksigen=300)
+         }
 def main():
-    print("== Stellar base ==")
-    print("1. Cek Status")
-    print("2. Cek cadangan oksigen")
-    print("3. Tambah tekanan udara")
-    print("4. jalankan mode darurat")
-    pilihan = input("Pilih opsi 1/2/3/4/: ")
+    while True:
+        print("== Stellar base ==")
+        print("1. Cek Status")
+        print("2. Distribusi Oksigen dan energi")
+        print("3. Tambah modul")
+        print("4. jalankan mode darurat")
+        print("5. Keluar")
+        pilihan = input("Pilih opsi: ")
+        match pilihan:
+            case "1":        
+                print("== Status ==")
+                
+            case "2": 
+                print("== Distribusi Oksigen dan Energi ==")
+                print("Distribusi oksigen dan energi sedang berlangsung...")
+            case "3":
+                print("== Tambah Modul ==")
+                print("Modul baru sedang ditambahkan...")
+            case "4":                  
+                print("== Mode Darurat ==")
+                print("Mode darurat diaktifkan! Semua sistem berjalan dengan prioritas tinggi!")
+            case "5":                  
+                print("Keluar dari program. Sampai jumpa!")
+                break
--- sistem.py
+++ sistem.py
@@ -1,14 +1,27 @@
-class oksigen:
+class RuangOksigen:
     def __init__(self,tekanan_udara,cadangan_oksigen,oksigen):
-        self._tekanan_udara = tekanan_udara
-        self.oksigen = oksigen
-        self._cadangan_oksigen = cadangan_oksigen
+        self.__tekanan_udara = tekanan_udara
+        self.__oksigen = oksigen
+        self.__cadangan_oksigen = cadangan_oksigen
     def info(self):
-        print(f"Tekanan udara: {self._tekanan_udara} kPa")
-        print(f"Cadangan oksigen: {self._cadangan_oksigen} liter")
-        print(f"Oksigen: {self.oksigen} liter")
+        print(f"Tekanan udara: {self.__tekanan_udara} kPa")
+        print(f"Cadangan oksigen: {self.__cadangan_oksigen} liter")
+        print(f"Oksigen: {self.__oksigen} liter")
 
-class laboratorium(oksigen):
-    def __init__(self, ):
-        super().__init__()
-    self.energi 
+    @property
+    def distribusi_oksigen(self):
+        return self.__oksigen
+
+    @distribusi_oksigen.setter
+    def distribusi_oksigen(self, jumlah):
+        if jumlah > self.__cadangan_oksigen:
+            print("Tidak cukup cadangan oksigen!")
+        else:
+            self.__cadangan_oksigen -= jumlah
+            self.__oksigen += jumlah
+
+
+class laboratorium(RuangOksigen):
+    def __init__(self, energi):
+        super().__init__(tekanan_udara=0, cadangan_oksigen=0, oksigen=0) 
+
