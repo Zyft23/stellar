9630b0ea2642df4d54cfe28a781fb8dfb76de269
Zyft23 <ariazulvanrc@gmail.com>
2026-05-18 02:54:45 +0000

    test

--- sistem.py
+++ sistem.py
@@ -2,6 +2,9 @@ from abc import ABC, abstractmethod
 class base(ABC):
     def __init__(self, tekanan_udara):
         self.__tekanan_udara = tekanan_udara
+    @property
+    def tekanan_udara(self):
+        return self.__tekanan_udara
     @abstractmethod
     def darurat(self):
         pass
@@ -12,11 +15,11 @@ class base(ABC):
 class Hunian(base):
     def __init__(self,cadangan_oksigen,oksigen):
         super().__init__(tekanan_udara=101.3)
-        self.__oksigen = oksigen
+        self.oksigen = oksigen
         self.__cadangan_oksigen = cadangan_oksigen
     @property
     def info(self):
-        return f"Hunian: Tekanan udara: {self._base__tekanan_udara} kPa, Oksigen: {self.__oksigen} liter, Cadangan Oksigen: {self.__cadangan_oksigen} liter"
+        return f"Hunian: Tekanan udara: {self.tekanan_udara} kPa, Oksigen: {self.oksigen} liter, Cadangan Oksigen: {self.__cadangan_oksigen} liter"
     
     @property
     def distribusi_oksigen(self):
@@ -36,9 +39,9 @@ class Hunian(base):
 
         print("Sistem darurat hunian diaktifkan!")
     def kondisi_aman(self):
-        if self.__tekanan_udara > 110:
+        if self.tekanan_udara > 110:
             print("Peringatan: Tekanan udara terlalu tinggi!")
-        elif self.__tekanan_udara <90:
+        elif self.tekanan_udara <90:
             print("Peringatan: Tekanan udara terlalu rendah!")
         elif self.__oksigen < 90:
             print("Peringatan: Oksigen hampir habis!")
@@ -58,11 +61,11 @@ class laboratorium(base):
 
         print("Sistem darurat laboratorium diaktifkan!")
     def kondisi_aman(self):
-        if self.__tekanan_udara > 110:
+        if self.tekanan_udara > 110:
             print("Peringatan: Tekanan udara terlalu tinggi!")
-        elif self.__tekanan_udara <90:
+        elif self.tekanan_udara < 90:
             print("Peringatan: Tekanan udara terlalu rendah!")
-        elif self.__oksigen < 90:
+        elif self.oksigen < 90:
             print("Peringatan: Oksigen hampir habis!")
         else:
             print("Kondisi aman. Tekanan udara dan oksigen dalam batas normal.")
